330bfd93901087657b00351a78bc806c62b8d5a5
Dmitriy Gonharov <my.webpage057@gmail.com>
2023-04-15 00:32:07 +0300

    add delete

---
+++ src/js/crud/d-delete.js
@@ -0,0 +1,12 @@
+// DELETE method - DELETE
+const BASE_URL = "https://jsonplaceholder.typicode.com";
+
+function removePost(postId) {
+    const url = `${BASE_URL}posts/${postId}`
+    const options = {
+        method: "DELETE",
+    }
+    return fetch(url,options).then(res=>res.json()).then(console.log)
+}
+removePost(3)
+removePost(4)
--- src/js/crud/u-patch.js
+++ src/js/crud/u-patch.js
@@ -1,4 +1,21 @@
-// Method PUT - заменяет обьект полностью
-// Method PATCH - обновляет параметры обьекта
+// Метод PATCH заменяет в существующем ресурсе значения переданные в теле запроса свойств. 
+// Метод PUT полностью заменяет ресурс.
 
+const BASE_URL = "https://jsonplaceholder.typicode.com";
 
+function updatePostById(object,postId) {
+    const options = {
+        method: "PATCH",
+        body: JSON.stringify(object),
+        headers: {
+            "Content-Type": "application/json",
+        }
+    }
+    return fetch(`${BASE_URL}/posts/${postId}`, options)
+    .then(res=> res.json())
+    .then(posts => console.log(posts))
+    .catch(e => console.log(e))
+}
+
+updatePostById({body: "CRUD is really awesome"}, 13)
+updatePostById({title: "Тестовая книга по Node.JS"}, 14)
