250e7cc099c48953a444f92eda57d934d44c1949
Dmitriy Gonharov <my.webpage057@gmail.com>
2023-04-18 23:48:42 +0300

    add try catch

--- src/js/async-await/async.js
+++ src/js/async-await/async.js
@@ -1,5 +1,5 @@
 
-
+// Синтаксис написания async/await
 // async function fn() { };
 
 // const fn = async function () {};
@@ -14,13 +14,39 @@
 //     async getName() {}
 // }
 
-async function getFruit(name) {
+function getFruit(name) {
     const fruits = {
         strawberry: "🍓",
         kiwi: "🥝",
         apple: "🍎"
     }
-    return Promise.resolve(fruits[name]);
+    // return Promise.resolve(fruits[name]);
+    return new Promise((resolve, reject) => {
+        setTimeout(()=> {
+            resolve(fruits[name])
+        }, 500)
+    })
+
 } 
 
-getFruit('apple').then(fruit=> console.log(fruit))
+async function asyncMakeFruit(name) {
+    console.time('asyncMakeFruit')
+try {
+    const apple = getFruit('apple');
+    console.log(apple);
+    const strawberry = getFruit('strawberry');
+    console.log(strawberry);
+    const kiwi = getFruit("kiwi");
+    console.log(kiwi);
+
+    const fruitsAll = await Promise.all([apple,strawberry,kiwi])
+    console.log(fruitsAll) 
+    
+} catch (error) {
+    console.log(error);
+}
+
+
+    console.timeEnd('asyncMakeFruit')
+}
+asyncMakeFruit()
